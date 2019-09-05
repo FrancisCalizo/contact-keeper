cedd26e0aba96e2473dc4c202852f9d54adf9077
Francis Calizo <francisacalizo@gmail.com>
2019-09-05 07:16:43 -0400

    Get users Contacts with mongoose and verify with JWT

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../models/User');
+const Contact = require('../models/Contact');
 
 // @route   GET api/contacts
 // @desc    Get all users contacts
 // @access  Private
-router.get('/', (req, res) => {
-  res.send('Get all user contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    // Find user contacts through req.user.id (in Auth Middleware)
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1
+    });
+    // Send the contact as a JSON object
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 // @route   POST api/contacts
