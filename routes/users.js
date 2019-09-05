221138b0ae1fa3a6011fa8f86cb82029631370b8
Francis Calizo <francisacalizo@gmail.com>
2019-09-04 22:53:23 -0400

    Error Handling and password Hashing using bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator/check');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/User');
 
@@ -9,6 +10,7 @@ const User = require('../models/User');
 // @access  Public
 router.post(
   '/',
+  // Express Validator to check the Request being sent (name, email, password)
   [
     check('name', 'Please add name')
       .not()
@@ -21,13 +23,41 @@ router.post(
       min: 6
     })
   ],
-  (req, res) => {
+  async (req, res) => {
+    // Return the errors above, if any
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed');
+    // Destructure the body that is sent
+    const { name, email, password } = req.body;
+
+    try {
+      // Mongoose method to find an email that mataches
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.status(400).json({ msg: 'User already exists' });
+      }
+
+      user = new User({
+        name: name,
+        email: email,
+        password: password
+      });
+
+      // Hash|Encrypt Password before saving to DB
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User Saved');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
