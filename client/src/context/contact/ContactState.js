47055a3c1c9fcbe52018214688a4e9bd24bab479
Francis Calizo <francisacalizo@gmail.com>
2019-09-09 23:30:02 -0400

    Filter and Clear Filter functionality

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,7 +37,8 @@ const ContactState = props => {
         type: 'professional'
       },
     ],
-    current: null
+    current: null,
+    filtered: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -73,6 +74,7 @@ const ContactState = props => {
       type: CLEAR_CURRENT
     })
   }
+
   // Update Contact
   const updateContact = contact => {
     dispatch({
@@ -82,18 +84,32 @@ const ContactState = props => {
   }
 
   // Filter Contacts
+  const filterContacts = text => {
+    dispatch({
+      type: FILTER_CONTACTS,
+      payload: text
+    })
+  }
 
   // Clear Filter
+  const clearFilter = () => {
+    dispatch({
+      type: CLEAR_FILTER
+    })
+  }
 
   return (
     <ContactContext.Provider value= {{
       contacts: state.contacts,
       current: state.current,
+      filtered: state.filtered,
       addContact,
       deleteContact,
       setCurrent,
       clearCurrent,
-      updateContact
+      updateContact,
+      filterContacts,
+      clearFilter
     }}>
       {props.children}
     </ContactContext.Provider>
