b4d8735dc3da44f48b7d7bbd7c952f826946ff41
Francis Calizo <francisacalizo@gmail.com>
2019-09-23 21:03:58 -0400

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,9 +10,20 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the Contact Keeper API' })
-);
+// Remove this in Production
+// app.get('/', (req, res) =>
+//   res.json({ msg: 'Welcome to the Contact Keeper API' })
+// );
+
+// Server Static Assets in Production
+if (process.env.NODE_ENV === 'production') {
+  // Set Static Folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
